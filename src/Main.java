9aaae2accbc39f078ed5c68cedac9b5acd3ba591
G2309 <106942151+G2309@users.noreply.github.com>
2023-03-29 18:50:48 -0600

    Main final

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,46 @@
 
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		VectorHeap<Process> vector = new VectorHeap<Process>();
+		PriorityQueueFactory factory = new PriorityQueueFactory();
 		Reader reader = new Reader();
-		reader.Read(vector);
-		int size = vector.size();
-		System.out.println("Bienvenido, estos son los procesos del CPU ordenados en base a su prioridad: ");
-		
-		for (int i =0; i<size; i++) {
-			Process process = vector.remove();
-			System.out.println(process.getProcessName() +","+process.getUsername()+","+process.getNice() +"," + "PR =" + process.getPR());
+		Scanner sc = new Scanner(System.in);
+		boolean b = true;
+		while (b) {
+			System.out
+					.println("\n\n\t\tSeleccione la implementacion que desea usar: \n\t1. VectorHeap\n\t2. PriorityQueue\n\t3. Terminar el programa");
+			String option = sc.nextLine();
+			if (option.equals("1")) {
+				IPriorityQueue<Process> vector = factory.createPQ(PriorityQueueFactory.PQueue.VectorHeap);
+				reader.Read(vector);
+				int size = vector.size();
+				System.out.println("Bienvenido, estos son los procesos del CPU ordenados en base a su prioridad: ");
+				for (int i = 0; i < size; i++) {
+					Process process = vector.remove();
+					System.out.println(process.getProcessName() + "," + process.getUsername() + "," + process.getNice()
+							+ "," + "PR =" + process.getPR());
+				}
+			} 
+			else if (option.equals("2")) 
+			{
+				IPriorityQueue<Process> PQ = factory.createPQ(PriorityQueueFactory.PQueue.PQueue);
+				reader.Read(PQ);
+				int size = PQ.size();
+				for (int i = 0; i < size; i++) {
+					Process process = PQ.remove();
+					System.out.println(process.getProcessName() + "," + process.getUsername() + "," + process.getNice()
+							+ "," + "PR =" + process.getPR());
+				}
+			} 
+			else if (option.equals("3")) 
+			{
+				b=false;
+			} 
+			else 
+			{
+				System.out.println("\n\t*** Opcion no valida, intente nuevamente ***\n\n");
+			} 
 		}
-
-
 	}
-
 }
